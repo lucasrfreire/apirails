fa42b8638f6059df9ea7c3b72ab2642007ab4d73
Lucas R Freire <lucas@localhost.localdomain>
2020-05-24 09:41:30 -0300

    Configurando arquivo de rotas para API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Forma de organizar as rotas em "categorias" criar uma pasta com nome da namespace la em controllers
+  # Primeiro parametro cria o path /api
+  # Segundo parametro diz que todo mundo aqui dentro vai trabalhar no formato json, para nao ter que especificar em todos
+  # Terceiro parametro diz que para acessar essa rota o subdominio tem que ser api, exemplo: api.site.com/api/teste
+  # Quarto paramentro e para nao ficar api repetido, agora a url fica assim: api.site.com/teste
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
+
+  end
 end
